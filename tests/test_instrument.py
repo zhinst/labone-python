a47552b1774dd964a6356f2db95cab02b6773c3e
Tobias Ahrens <tobias.ahrens@zhinst.com>
2024-01-03 16:32:47 +0100

    refactor the instrument unit tests to use the session mock

--- tests/test_instrument.py
+++ tests/test_instrument.py
@@ -1,127 +1,65 @@
-from unittest.mock import create_autospec, patch
+from unittest.mock import MagicMock, patch
 
 import pytest
-from labone.core import (
-    KernelSession,
-)
 from labone.errors import LabOneError
 from labone.instrument import Instrument
-from labone.nodetree.node import NodeTreeManager
+from labone.mock import AutomaticSessionFunctionality, spawn_hpk_mock
 
-from tests.test_dataserver import MockModelNode
-
-
-class MockInstrument(Instrument):
-    def __init__(self):
-        super().__init__(
-            serial="serial",
-            model_node=MockModelNode(),
-        )
-
-
-def test_unerlying_server():
-    dataserver = MockInstrument()
-    dataserver._tree_manager = create_autospec(NodeTreeManager)
-    assert dataserver.kernel_session == dataserver._tree_manager.session
-
-
-def test_repr():
-    serial = "dev1234"
-    mock_instrument = MockInstrument()
-    mock_instrument._serial = serial
-    assert serial in repr(mock_instrument)
+from tests.mock_server_for_testing import get_mocked_node
 
 
 @pytest.mark.asyncio()
-async def test_connect_device():
-    with patch.object(
-        KernelSession,
-        "create",
-        autospec=True,
-        return_value="session",
-    ) as create_mock, patch(
-        "labone.instrument.DeviceKernelInfo",
-        autospec=True,
-        return_value="kernel_info",
-    ) as kernelinfo_mock, patch(
-        "labone.instrument.ServerInfo",
-        autospec=True,
-        return_value="server_info",
-    ) as serverinfo_mock, patch(
-        "labone.instrument.Instrument",
-        autospec=True,
-        return_value="server_info",
-    ) as init_mock, patch(
-        "labone.instrument.construct_nodetree",
-        autospec=True,
-        return_value="node",
-    ) as construct_mock:
-        await Instrument.create(
-            "serial",
-            host="host",
-            port="port",
-            interface="interface",
-            custom_parser="custom_parser",
-        )
-
-        kernelinfo_mock.assert_called_once_with(
-            device_id="serial",
-            interface="interface",
-        )
-        serverinfo_mock.assert_called_once_with(host="host", port="port")
-        create_mock.assert_called_once_with(
-            kernel_info="kernel_info",
-            server_info="server_info",
-        )
-        construct_mock.assert_called_once_with(
-            "session",
-            custom_parser="custom_parser",
-        )
-        init_mock.assert_called_once_with(
-            serial="serial",
-            model_node="node",
-        )
+@patch("labone.instrument.KernelSession", autospec=True)
+async def test_connect_device(kernel_session):
+    session = await spawn_hpk_mock(AutomaticSessionFunctionality({}))
+    kernel_session.create.return_value = session
+    instrument = await Instrument.create("dev1234", host="testee")
+    assert instrument.kernel_session == session
+    assert instrument.serial == "dev1234"
+    kernel_session.create.assert_awaited_once()
+    assert kernel_session.create.call_args.kwargs["kernel_info"].device_id == "dev1234"
+    assert kernel_session.create.call_args.kwargs["kernel_info"].interface == ""
+    assert kernel_session.create.call_args.kwargs["server_info"].host == "testee"
+    assert kernel_session.create.call_args.kwargs["server_info"].port == 8004
 
 
 @pytest.mark.asyncio()
-async def test_connect_device_raises():
-    with patch.object(
-        KernelSession,
-        "create",
-        autospec=True,
-        return_value="session",
-    ) as create_mock, patch(
-        "labone.instrument.DeviceKernelInfo",
-        autospec=True,
-        return_value="kernel_info",
-    ) as kernelinfo_mock, patch(
-        "labone.instrument.ServerInfo",
-        autospec=True,
-        return_value="server_info",
-    ) as serverinfo_mock, patch(
-        "labone.instrument.construct_nodetree",
-        autospec=True,
-        return_value="node",
-        side_effect=LabOneError(),
-    ) as construct_mock:
-        with pytest.raises(LabOneError):
-            await Instrument.create(
-                "serial",
-                host="host",
-                port="port",
-                interface="interface",
-                custom_parser="custom_parser",
-            )
-        kernelinfo_mock.assert_called_once_with(
-            device_id="serial",
-            interface="interface",
-        )
-        serverinfo_mock.assert_called_once_with(host="host", port="port")
-        create_mock.assert_called_once_with(
-            kernel_info="kernel_info",
-            server_info="server_info",
-        )
-        construct_mock.assert_called_once_with(
-            "session",
-            custom_parser="custom_parser",
-        )
+@patch("labone.instrument.KernelSession", autospec=True)
+async def test_connect_device_custom_default_args(kernel_session):
+    session = await spawn_hpk_mock(AutomaticSessionFunctionality({}))
+    kernel_session.create.return_value = session
+    instrument = await Instrument.create(
+        "dev1234",
+        host="testee",
+        port=8888,
+        interface="wifi",
+    )
+    assert instrument.kernel_session == session
+    assert instrument.serial == "dev1234"
+    kernel_session.create.assert_awaited_once()
+    assert kernel_session.create.call_args.kwargs["kernel_info"].device_id == "dev1234"
+    assert kernel_session.create.call_args.kwargs["kernel_info"].interface == "wifi"
+    assert kernel_session.create.call_args.kwargs["server_info"].host == "testee"
+    assert kernel_session.create.call_args.kwargs["server_info"].port == 8888
+
+
+@pytest.mark.asyncio()
+@patch("labone.instrument.KernelSession", autospec=True)
+async def test_connect_device_error(kernel_session):
+    session = MagicMock()
+    kernel_session.create.return_value = session
+    session.list_nodes_info.side_effect = LabOneError()
+    with pytest.raises(LabOneError):
+        await Instrument.create("dev1234", host="testee")
+
+
+@pytest.mark.asyncio()
+async def test_underlying_server():
+    instrument = Instrument(serial="dev1234", model_node=await get_mocked_node({}))
+    assert instrument.kernel_session == instrument._tree_manager.session
+
+
+@pytest.mark.asyncio()
+async def test_repr():
+    instrument = Instrument(serial="dev1234", model_node=await get_mocked_node({}))
+    assert instrument.serial in repr(instrument)
